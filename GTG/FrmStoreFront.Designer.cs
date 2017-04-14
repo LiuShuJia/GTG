c6bfe59a9c99848bcd19bfc6695a251e14ee291e
李孔强 <www.1019796392@qq.com>
2017-04-14 17:04:49 +0800

    修改了店面销售界面，增加了一个listview控件用于显示数据，增加了五个label控件和文本控件用于显示查询条件，两个按钮分别用于执行查询和增加的命令

--- GTG/FrmStoreFront.Designer.cs
+++ GTG/FrmStoreFront.Designer.cs
@@ -29,76 +29,219 @@
         private void InitializeComponent()
         {
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
-            this.店铺信息ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.客户信息表ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.提货单信息表ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.店铺信息ToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
-            this.店员信息ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.销售单ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiSalesStore = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiClerk = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiClientele = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiBillOfLading = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiSalesTicket = new System.Windows.Forms.ToolStripMenuItem();
+            this.lstTable = new System.Windows.Forms.ListView();
+            this.btnSelect = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.lblText1 = new System.Windows.Forms.Label();
+            this.lblText2 = new System.Windows.Forms.Label();
+            this.cmbName = new System.Windows.Forms.ComboBox();
+            this.lblDay = new System.Windows.Forms.Label();
+            this.lblSex = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.dtpTime = new System.Windows.Forms.DateTimePicker();
+            this.cmbSex = new System.Windows.Forms.ComboBox();
+            this.txtCardID = new System.Windows.Forms.TextBox();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // menuStrip1
             // 
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.店铺信息ToolStripMenuItem,
-            this.客户信息表ToolStripMenuItem,
-            this.提货单信息表ToolStripMenuItem,
-            this.销售单ToolStripMenuItem});
+            this.tsmiSalesStore,
+            this.tsmiClientele,
+            this.tsmiBillOfLading,
+            this.tsmiSalesTicket});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(427, 25);
+            this.menuStrip1.Size = new System.Drawing.Size(479, 25);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
-            // 店铺信息ToolStripMenuItem
+            // tsmiSalesStore
             // 
-            this.店铺信息ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.店铺信息ToolStripMenuItem1,
-            this.店员信息ToolStripMenuItem});
-            this.店铺信息ToolStripMenuItem.Name = "店铺信息ToolStripMenuItem";
-            this.店铺信息ToolStripMenuItem.Size = new System.Drawing.Size(68, 21);
-            this.店铺信息ToolStripMenuItem.Text = "店铺信息";
+            this.tsmiSalesStore.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsmiClerk});
+            this.tsmiSalesStore.Name = "tsmiSalesStore";
+            this.tsmiSalesStore.Size = new System.Drawing.Size(68, 21);
+            this.tsmiSalesStore.Text = "店铺信息";
+            this.tsmiSalesStore.Click += new System.EventHandler(this.tsmi_Click);
             // 
-            // 客户信息表ToolStripMenuItem
+            // tsmiClerk
             // 
-            this.客户信息表ToolStripMenuItem.Name = "客户信息表ToolStripMenuItem";
-            this.客户信息表ToolStripMenuItem.Size = new System.Drawing.Size(80, 21);
-            this.客户信息表ToolStripMenuItem.Text = "客户信息表";
+            this.tsmiClerk.Name = "tsmiClerk";
+            this.tsmiClerk.Size = new System.Drawing.Size(152, 22);
+            this.tsmiClerk.Text = "店员信息";
+            this.tsmiClerk.Click += new System.EventHandler(this.tsmiClerk_Click);
             // 
-            // 提货单信息表ToolStripMenuItem
+            // tsmiClientele
             // 
-            this.提货单信息表ToolStripMenuItem.Name = "提货单信息表ToolStripMenuItem";
-            this.提货单信息表ToolStripMenuItem.Size = new System.Drawing.Size(92, 21);
-            this.提货单信息表ToolStripMenuItem.Text = "提货单信息表";
+            this.tsmiClientele.Name = "tsmiClientele";
+            this.tsmiClientele.Size = new System.Drawing.Size(80, 21);
+            this.tsmiClientele.Text = "客户信息表";
+            this.tsmiClientele.Click += new System.EventHandler(this.tsmiClientele_Click);
             // 
-            // 店铺信息ToolStripMenuItem1
+            // tsmiBillOfLading
             // 
-            this.店铺信息ToolStripMenuItem1.Name = "店铺信息ToolStripMenuItem1";
-            this.店铺信息ToolStripMenuItem1.Size = new System.Drawing.Size(152, 22);
-            this.店铺信息ToolStripMenuItem1.Text = "店铺信息";
+            this.tsmiBillOfLading.Name = "tsmiBillOfLading";
+            this.tsmiBillOfLading.Size = new System.Drawing.Size(92, 21);
+            this.tsmiBillOfLading.Text = "提货单信息表";
+            this.tsmiBillOfLading.Click += new System.EventHandler(this.tsmiBillOfLading_Click);
             // 
-            // 店员信息ToolStripMenuItem
+            // tsmiSalesTicket
             // 
-            this.店员信息ToolStripMenuItem.Name = "店员信息ToolStripMenuItem";
-            this.店员信息ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
-            this.店员信息ToolStripMenuItem.Text = "店员信息";
+            this.tsmiSalesTicket.Name = "tsmiSalesTicket";
+            this.tsmiSalesTicket.Size = new System.Drawing.Size(92, 21);
+            this.tsmiSalesTicket.Text = "销售单信息表";
+            this.tsmiSalesTicket.Click += new System.EventHandler(this.tsmiSalesTicket_Click);
             // 
-            // 销售单ToolStripMenuItem
+            // lstTable
             // 
-            this.销售单ToolStripMenuItem.Name = "销售单ToolStripMenuItem";
-            this.销售单ToolStripMenuItem.Size = new System.Drawing.Size(92, 21);
-            this.销售单ToolStripMenuItem.Text = "销售单信息表";
+            this.lstTable.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lstTable.FullRowSelect = true;
+            this.lstTable.GridLines = true;
+            this.lstTable.Location = new System.Drawing.Point(0, 25);
+            this.lstTable.Name = "lstTable";
+            this.lstTable.Size = new System.Drawing.Size(479, 268);
+            this.lstTable.TabIndex = 1;
+            this.lstTable.UseCompatibleStateImageBehavior = false;
+            this.lstTable.View = System.Windows.Forms.View.Details;
+            // 
+            // btnSelect
+            // 
+            this.btnSelect.Location = new System.Drawing.Point(381, 358);
+            this.btnSelect.Name = "btnSelect";
+            this.btnSelect.Size = new System.Drawing.Size(75, 23);
+            this.btnSelect.TabIndex = 2;
+            this.btnSelect.Text = "查询";
+            this.btnSelect.UseVisualStyleBackColor = true;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(381, 452);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 3;
+            this.btnAdd.Text = "增加";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // lblText1
+            // 
+            this.lblText1.AutoSize = true;
+            this.lblText1.Location = new System.Drawing.Point(74, 323);
+            this.lblText1.Name = "lblText1";
+            this.lblText1.Size = new System.Drawing.Size(77, 12);
+            this.lblText1.TabIndex = 5;
+            this.lblText1.Text = "按名称查询：";
+            // 
+            // lblText2
+            // 
+            this.lblText2.AutoSize = true;
+            this.lblText2.Location = new System.Drawing.Point(74, 369);
+            this.lblText2.Name = "lblText2";
+            this.lblText2.Size = new System.Drawing.Size(77, 12);
+            this.lblText2.TabIndex = 6;
+            this.lblText2.Text = "按地址查询：";
+            // 
+            // cmbName
+            // 
+            this.cmbName.FormattingEnabled = true;
+            this.cmbName.Items.AddRange(new object[] {
+            "查询全部"});
+            this.cmbName.Location = new System.Drawing.Point(157, 320);
+            this.cmbName.Name = "cmbName";
+            this.cmbName.Size = new System.Drawing.Size(159, 20);
+            this.cmbName.TabIndex = 8;
+            // 
+            // lblDay
+            // 
+            this.lblDay.AutoSize = true;
+            this.lblDay.Location = new System.Drawing.Point(74, 413);
+            this.lblDay.Name = "lblDay";
+            this.lblDay.Size = new System.Drawing.Size(77, 12);
+            this.lblDay.TabIndex = 9;
+            this.lblDay.Text = "按日期查询：";
+            // 
+            // lblSex
+            // 
+            this.lblSex.AutoSize = true;
+            this.lblSex.Location = new System.Drawing.Point(74, 452);
+            this.lblSex.Name = "lblSex";
+            this.lblSex.Size = new System.Drawing.Size(77, 12);
+            this.lblSex.TabIndex = 10;
+            this.lblSex.Text = "按性别查询：";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(38, 494);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(113, 12);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "按身份证号码查询：";
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Location = new System.Drawing.Point(157, 366);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(159, 21);
+            this.txtAddress.TabIndex = 12;
+            // 
+            // dtpTime
+            // 
+            this.dtpTime.Location = new System.Drawing.Point(157, 407);
+            this.dtpTime.Name = "dtpTime";
+            this.dtpTime.Size = new System.Drawing.Size(159, 21);
+            this.dtpTime.TabIndex = 13;
+            // 
+            // cmbSex
+            // 
+            this.cmbSex.FormattingEnabled = true;
+            this.cmbSex.Items.AddRange(new object[] {
+            "查询全部",
+            "男",
+            "女"});
+            this.cmbSex.Location = new System.Drawing.Point(157, 449);
+            this.cmbSex.Name = "cmbSex";
+            this.cmbSex.Size = new System.Drawing.Size(159, 20);
+            this.cmbSex.TabIndex = 14;
+            // 
+            // txtCardID
+            // 
+            this.txtCardID.Location = new System.Drawing.Point(157, 491);
+            this.txtCardID.Name = "txtCardID";
+            this.txtCardID.Size = new System.Drawing.Size(159, 21);
+            this.txtCardID.TabIndex = 16;
             // 
             // FrmStoreFront
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(427, 261);
+            this.ClientSize = new System.Drawing.Size(479, 526);
+            this.Controls.Add(this.txtCardID);
+            this.Controls.Add(this.cmbSex);
+            this.Controls.Add(this.dtpTime);
+            this.Controls.Add(this.txtAddress);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblSex);
+            this.Controls.Add(this.lblDay);
+            this.Controls.Add(this.cmbName);
+            this.Controls.Add(this.lblText2);
+            this.Controls.Add(this.lblText1);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnSelect);
+            this.Controls.Add(this.lstTable);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "FrmStoreFront";
             this.Text = "店面销售界面";
+            this.Load += new System.EventHandler(this.FrmStoreFront_Load);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
@@ -109,11 +252,23 @@
         #endregion
 
         private System.Windows.Forms.MenuStrip menuStrip1;
-        private System.Windows.Forms.ToolStripMenuItem 店铺信息ToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem 店铺信息ToolStripMenuItem1;
-        private System.Windows.Forms.ToolStripMenuItem 店员信息ToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem 客户信息表ToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem 提货单信息表ToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem 销售单ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tsmiSalesStore;
+        private System.Windows.Forms.ToolStripMenuItem tsmiClerk;
+        private System.Windows.Forms.ToolStripMenuItem tsmiClientele;
+        private System.Windows.Forms.ToolStripMenuItem tsmiBillOfLading;
+        private System.Windows.Forms.ToolStripMenuItem tsmiSalesTicket;
+        private System.Windows.Forms.ListView lstTable;
+        private System.Windows.Forms.Button btnSelect;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Label lblText1;
+        private System.Windows.Forms.Label lblText2;
+        private System.Windows.Forms.ComboBox cmbName;
+        private System.Windows.Forms.Label lblDay;
+        private System.Windows.Forms.Label lblSex;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.DateTimePicker dtpTime;
+        private System.Windows.Forms.ComboBox cmbSex;
+        private System.Windows.Forms.TextBox txtCardID;
     }
 }
--- GTG/FrmStoreFront.cs
+++ GTG/FrmStoreFront.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace GTG
 {
@@ -15,6 +16,93 @@ namespace GTG
         public FrmStoreFront()
         {
             InitializeComponent();
+        }
+        public FrmStoreFront(string CCardID)
+        {
+            this.CCardID = CCardID;
+            InitializeComponent();
+        }
+        private DBHelper helper = new DBHelper();
+        private string CCardID;
+        private void tsmiClerk_Click(object sender, EventArgs e)
+        {
+            string SName = this.cmbName.Text.Trim();
+            string SAddress= this.txtAddress.Text.Trim();
+            string strSQL = "select * from CLerk where (SName=@SName or len(@SName)=0)and (SAddress=@SAddress or len(@SAddress)=0)";
+            using (IDataReader reader = helper.ExecuteReader(strSQL, CommandType.Text,
+                  new SqlParameter("@SName", this.cmbName.Text.Trim()),
+                   new SqlParameter("@SAddress", this.txtAddress.Text.Trim())))
+            {
+                while (reader.Read())
+                {
+                    SName= reader.GetString(reader.GetOrdinal("SName"));
+                    this.cmbName.Items.Add(SName);
+                    SAddress = reader.GetString(reader.GetOrdinal("SAddress"));
+                    this.cmbText2.Items.Add(SName);
+                    
+
+                    ListViewItem lst = new ListViewItem();
+                    lst.SubItems.Add(reader.GetString(reader.GetOrdinal("SName")));
+                    lst.SubItems.Add(reader.GetString(reader.GetOrdinal("SAddress")));
+                    lst.SubItems.Add(reader.GetString(reader.GetOrdinal("SManagerName")));
+                    lst.SubItems.Add(reader.GetString(reader.GetOrdinal("SPhone")));
+
+                    this.lstTable.Items.Add(lst);
+                }
+                reader.Close();
+            }
+        }
+
+        private void tsmi_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tsmiStore_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FrmStoreFront_Load(object sender, EventArgs e)
+        {
+            string SName = this.cmbName.Text.Trim(); ;
+            string SAddress = this.txtAddress.Text.Trim();
+            string strSQL = "select * from CLerk where SID=@SID";
+            using (IDataReader reader = helper.ExecuteReader(strSQL, CommandType.Text))
+            {
+                while (reader.Read())
+                {
+                    ListViewItem lst = new ListViewItem();
+                    lst.SubItems.Add(reader.GetString(reader.GetOrdinal("SName")));
+                    lst.SubItems.Add(reader.GetString(reader.GetOrdinal("SAddress")));
+                    lst.SubItems.Add(reader.GetString(reader.GetOrdinal("SManagerName")));
+                    lst.SubItems.Add(reader.GetString(reader.GetOrdinal("SPhone")));
+
+                    this.lstTable.Items.Add(lst);
+                }
+                reader.Close();
+            }
+
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)                                //增加信息
+        {
+            
+        }
+
+        private void tsmiBillOfLading_Click(object sender, EventArgs e)                            //提货表
+        {
+            //lblText1.Text = "按日期查询：";
+        }
+
+        private void tsmiSalesTicket_Click(object sender, EventArgs e)                              //销售表
+        {
+            //lblText1.Text = "按日期查询：";
+        }
+
+        private void tsmiClientele_Click(object sender, EventArgs e)                            //客户信息表
+        {
+            //lblText1.Text = "按姓名查询：";
         }
     }
 }
