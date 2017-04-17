70a62cc39b55565a64d62921ce38974a5a9de1c3
叶宜峰 <2909466150@qq.com>
2017-04-17 22:01:59 +0800

    1

--- GTG/FrmGoods.cs
+++ GTG/FrmGoods.cs
@@ -24,13 +24,40 @@ namespace GTG
         {
             string gname = txtName.Text;
             string gstyle = txtStyle.Text;
+            this.listView1.Items.Clear();
+            string strSQL = "select * from Goods where (CHARINDEX(@gname,gname)>0 or len(@gname)=0) and(CHARINDEX(@gstyle,gstyle)>0 or len(@gstyle)=0)";
+            IDataReader reader = helper.ExecuteReader(strSQL, CommandType.Text,
+                new SqlParameter("@gname", gname),
+                new SqlParameter("@gstyle", gstyle));
 
-        
+            while (reader.Read())
+            {
+
+                ListViewItem lst = new ListViewItem(reader.GetString(reader.GetOrdinal("")));
+                lst.SubItems.Add(reader.GetString(reader.GetOrdinal("")));
+                lst.SubItems.Add(reader.GetString(reader.GetOrdinal("")));
+
+                this.listView1.Items.Add(lst);
+            }
+            reader.Close();
+
         }
 
         private void FrmGoods_Load(object sender, EventArgs e)
         {
-           
+            this.listView1.Items.Clear();
+            string strSQL = "select * from Goods";
+            IDataReader reader = helper.ExecuteReader(strSQL, CommandType.Text);
+            while (reader.Read())
+            {
+
+                ListViewItem lst = new ListViewItem(reader.GetString(reader.GetOrdinal("")));
+                lst.SubItems.Add(reader.GetString(reader.GetOrdinal("")));
+                lst.SubItems.Add(reader.GetString(reader.GetOrdinal("")));
+
+                this.listView1.Items.Add(lst);
+            }
+            reader.Close();
         }
     }
 }
