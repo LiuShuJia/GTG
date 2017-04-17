5ba7cc13e3ccee880434757a8885acbf5e91ddf3
叶宜峰 <2909466150@qq.com>
2017-04-17 23:06:19 +0800

    送货表

---
+++ GTG/FrmDeliveryNote.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GTG
+{
+    public partial class FrmDeliveryNote : Form
+    {
+        public FrmDeliveryNote()
+        {
+            InitializeComponent();
+        }
+
+        private void btnQue_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FrmDeliveryNote_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
